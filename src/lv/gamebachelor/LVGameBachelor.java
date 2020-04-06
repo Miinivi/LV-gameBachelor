19e870a1474a074595d70e94838c0d905b54662d
Miinivi <venianelam@gmail.com>
2020-04-06 18:40:21 +0200

    Correction des chemins de fichier

---
+++ src/lv/gamebachelor/LVGameBachelor.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lv.gamebachelor;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Lam & Rodrigues
+ */
+public class LVGameBachelor extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        StackPane root = new StackPane();
+        BorderPane gameContent = new GameContentApp();
+        root.getChildren().add(gameContent);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
