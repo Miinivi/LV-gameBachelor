f9936d620197e3b81ca053fdaac778dacfae1438
Miinivi <45562698+Miinivi@users.noreply.github.com>
2020-04-06 17:48:53 +0200

    Add files via upload

--- LV-gameBachelor/src/lv/gamebachelor/LVGameBachelor.java
+++ LV-gameBachelor/src/lv/gamebachelor/LVGameBachelor.java
@@ -6,10 +6,8 @@
 package lv.gamebachelor;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -21,18 +19,9 @@ public class LVGameBachelor extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        BorderPane gameContent = new GameContentApp();
+        root.getChildren().add(gameContent);
         
         Scene scene = new Scene(root, 300, 250);
         
